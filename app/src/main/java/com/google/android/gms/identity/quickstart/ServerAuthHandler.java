bab5f5bbb963840abc5143fba52c45a8f9f24bb3
Jessica Chen <xihua.chen@nytimes.com>
2015-08-07 19:07:40 -0400

    first commit

---
+++ app/src/main/java/com/google/android/gms/identity/quickstart/ServerAuthHandler.java
@@ -0,0 +1,181 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.gms.identity.quickstart;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Logic to handle the ServerAuthCode exchange for offline access during sign-in.
+ */
+public class ServerAuthHandler implements GoogleApiClient.ServerAuthCodeCallbacks {
+
+    private static final String TAG = ServerAuthHandler.class.getSimpleName();
+
+    // Base URL for your token exchange server, no trailing slash.
+    private static final String SERVER_BASE_URL = "SERVER_BASE_URL";
+
+    // URL where the client should GET the scopes that the server would like granted
+    // before asking for a serverAuthCode.
+    private static final String SELECT_SCOPES_URL = SERVER_BASE_URL + "/selectscopes";
+
+    // URL where the client should POST the serverAuthCode so that the server can exchange
+    // it for a refresh token.
+    private static final String EXCHANGE_TOKEN_URL = SERVER_BASE_URL + "/exchangetoken";
+
+    // Used to mock the state of a server that would receive an auth code to exchange
+    // for a refresh token,  If true, the client will assume that the server has the
+    // permissions it wants and will not send an auth code on sign in.  If false,
+    // the client will request offline access on sign in and send and new auth code
+    // to the server.  True by default because this sample does not implement a server
+    // so there would be nowhere to send the code.
+    private boolean mServerHasToken = true;
+
+    private Activity mActivity;
+    private Handler handler;
+
+    public ServerAuthHandler(Activity activity, Handler handler) {
+        mActivity = activity;
+        this.handler = handler;
+
+    }
+
+    // [START on_check_server_authorization]
+    @Override
+    public CheckResult onCheckServerAuthorization(String idToken, Set<Scope> scopeSet) {
+        Log.i(TAG, "Checking if server is authorized.");
+
+        // Check if the server has a token.  Since this callback executes in a background
+        // thread it is OK to do synchronous network access in this check.
+        boolean serverHasToken = serverHasTokenFor(idToken);
+        Log.i(TAG, "Server has token: " + String.valueOf(serverHasToken));
+
+        if (true) {
+            // Server does not have a valid refresh token, so request a new
+            // auth code which can be exchanged for one.  This will cause the user to see the
+            // consent dialog and be prompted to grant offline access.
+
+            // Ask the server which scopes it would like to have for offline access.  This
+            // can be distinct from the scopes granted to the client.  By getting these values
+            // from the server, you can change your server's permissions without needing to
+            // recompile the client application.
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(SELECT_SCOPES_URL);
+            HashSet<Scope> serverScopeSet = new HashSet<Scope>();
+            serverScopeSet.add(new Scope("https://www.googleapis.com/auth/userinfo.profile"));
+
+/*            try {
+                HttpResponse httpResponse = httpClient.execute(httpGet);
+                int responseCode = httpResponse.getStatusLine().getStatusCode();
+                String responseBody = EntityUtils.toString(httpResponse.getEntity());
+
+                // Convert the response to set of Scope objects.
+                if (responseCode == 200) {
+                    String[] scopeStrings = responseBody.split(" ");
+                    for (String scope : scopeStrings) {
+                        Log.i(TAG, "Server Scope: " + scope);
+                        serverScopeSet.add(new Scope(scope));
+                    }
+                } else {
+                    Log.e(TAG, "Error in getting server scopes: " + responseCode);
+                }
+
+            } catch (ClientProtocolException e) {
+                Log.e(TAG, "Error in getting server scopes.", e);
+            } catch (IOException e) {
+                Log.e(TAG, "Error in getting server scopes.", e);
+            }*/
+
+            // This tells GoogleApiClient that the server needs a new serverAuthCode with
+            // access to the scopes in serverScopeSet.  Note that we are not asking the server
+            // if it already has such a token because this is a sample application.  In reality,
+            // you should only do this on the first user sign-in or if the server loses or deletes
+            // the refresh token.
+            return CheckResult.newAuthRequiredResult(serverScopeSet);
+        } else {
+            // Server already has a valid refresh token with the correct scopes, no need to
+            // ask the user for offline access again.
+            return CheckResult.newAuthNotRequiredResult();
+        }
+    }
+    // [END on_check_server_authorization]
+
+    // [START on_upload_server_auth_code]
+    @Override
+    public boolean onUploadServerAuthCode(String idToken, String serverAuthCode) {
+        // Upload the serverAuthCode to the server, which will attempt to exchange it for
+        // a refresh token.  This callback occurs on a background thread, so it is OK
+        // to perform synchronous network access.  Returning 'false' will fail the
+        // GoogleApiClient.connect() call so if you would like the client to ignore
+        // server failures, always return true.
+        //((TextView) mActivity.findViewById(R.id.token_value)).setText("Token Value: " + serverAuthCode);
+        Bundle bundle = new Bundle();
+        bundle.putString("token", serverAuthCode);
+        Message message = new Message();
+        message.setData(bundle);
+        handler.sendMessage(message);
+
+        return true;
+    }
+    // [END on_upload_server_auth_code]
+
+   public void checkServerAuthConfiguration(String webClientId) {
+        // Check that the WEB_CLIENT_ID and SERVER_BASE_URL have been correctly
+        // set, otherwise server auth code flow will fail in strange ways.  If either
+        // is left to the default value, display a Dialog alerting the user.
+        if ("WEB_CLIENT_ID".equals(webClientId)) {
+            Log.w(TAG, "WEB_CLIENT_ID or SERVER_BASE_URL configured incorrectly.");
+            Dialog dialog = new AlertDialog.Builder(mActivity)
+                    .setMessage(mActivity.getString(R.string.configuration_error))
+                    .setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create();
+
+            dialog.show();
+        }
+    }
+
+    private boolean serverHasTokenFor(String idToken) {
+        // TODO: This method is a stub, in a real application we would make an HTTP
+        // request to the server here.
+        return mServerHasToken;
+    }
+
+    public void setServerHasToken(boolean serverHasToken) {
+        mServerHasToken = serverHasToken;
+    }
+
+}
